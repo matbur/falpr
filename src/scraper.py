6bb75920d99cd42d474da0ab93ba20119eec9c39
matbur95 <mateusz@burniak.pl>
2017-06-12 00:49:17 +0200

    Add more docs about model and scrapper

--- src/scraper.py
+++ src/scraper.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
+import json
 import os
-from multiprocessing.pool import Pool
+from multiprocessing.pool import ThreadPool as Pool
 from time import time
 
 import requests
@@ -32,7 +33,9 @@ def save(fn, data, bytes_=False):
 
 
 def worker(data):
-    fn, url, bytes_ = data
+    fn, url, *bytes_ = data
+    bytes_ = bytes_ is not None
+
     data = scrap(url, bytes_)
 
     if data is None:
@@ -42,19 +45,36 @@ def worker(data):
     logger.info(url)
 
 
+def platesmania(gallery_dir):
+    url = 'http://platesmania.com/pl/gallery'
+    gallery_range = range(5762)
+    fns = [os.path.join(gallery_dir, f'gallery-{i}.html') for i in gallery_range]
+    urls = [f'{url}-{i}' for i in gallery_range]
+    return zip(fns, urls)
+
+
+def rejestracja_blog(gallery_dir):
+    url = 'http://rejestracja.blog.pl/page/{}/'
+    gallery_range = range(800)
+    fns = [os.path.join(gallery_dir, f'gallery-{i}.html') for i in gallery_range]
+    urls = [url.format(i) for i in gallery_range]
+    return zip(fns, urls)
+
+
 def main():
     t = time()
 
     data_dir = os.path.join('..', 'data')
-    gallery_dir = os.path.join(data_dir, 'gallery')
-    url = 'http://platesmania.com/pl/gallery'
+    images_dir = os.path.join(data_dir, 'images')
 
-    gallery_range = range(5762)
-    fns = [os.path.join(gallery_dir, f'gallery-{i}.html') for i in gallery_range]
-    urls = [f'{url}-{i}' for i in gallery_range]
-    data = zip(fns, urls)
+    with open('images.json') as f:
+        data = list(list(i) for i in json.load(f).items())
 
-    with Pool(4) as pool:
+    for i in data:
+        i[0] = os.path.join(images_dir, f'{i[0]}.jpg')
+        i.append(True)
+
+    with Pool() as pool:
         pool.map(worker, data)
 
     logger.info(f'Done in {time() - t}')
